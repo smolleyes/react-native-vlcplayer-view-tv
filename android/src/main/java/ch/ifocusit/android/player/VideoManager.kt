785e2583096d402ac08163af9e6b427b9b2463f6
smo <test@test.com>
2025-01-31 16:15:18 +0100

    sdf

--- android/src/main/java/ch/ifocusit/android/player/VideoManager.kt
+++ android/src/main/java/ch/ifocusit/android/player/VideoManager.kt
@@ -1,89 +1,96 @@
 package ch.ifocusit.android.player
 
 import android.app.Activity
-import android.content.Context
 import android.content.pm.ActivityInfo
-import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import com.facebook.react.bridge.ReactContext
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
 
-data class VideoSource(
-    val uri: String,
-    val time: Long = 0
-)
+class VideoManager : SimpleViewManager<VideoView>() {
+  override fun getName(): String = "VideoView"
 
-class VideoManager(private val context: Context) {
-    private var player: VlcPlayer? = null
-    private var view: VideoView? = null
-    private var fullscreen = false
-    private var originalOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-    private var originalParent: ViewGroup? = null
-    private var originalLayoutParams: ViewGroup.LayoutParams? = null
-    private var fullscreenContainer: FrameLayout? = null
+  override fun createViewInstance(reactContext: ThemedReactContext): VideoView {
+    return VideoView(reactContext)
+  }
 
-    fun createPlayer(source: VideoSource): VlcPlayer {
-        player = VlcPlayer(context, source)
-        return player!!
+  @ReactProp(name = "fullscreen", defaultBoolean = false)
+  fun setFullscreen(videoView: VideoView, fullscreen: Boolean) {
+    videoView.post {
+      videoView.toggleFullscreen(fullscreen)
     }
-
-    fun setView(view: VideoView) {
-        this.view = view
-        player?.let { view.addView(it) }
-    }
-
-    fun toggleFullscreen() {
-        val activity = (context as ReactContext).currentActivity ?: return
-        if (fullscreen) {
-            exitFullscreen(activity)
-        } else {
-            enterFullscreen(activity)
-        }
-    }
-
-    private fun enterFullscreen(activity: Activity) {
-        val decorView = activity.window.decorView as ViewGroup
-        val view = view ?: return
-
-        originalOrientation = activity.requestedOrientation
-        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-
-        originalParent = view.parent as? ViewGroup
-        originalLayoutParams = view.layoutParams
-
-        originalParent?.removeView(view)
-
-        fullscreenContainer = FrameLayout(context).apply {
-            layoutParams = FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            addView(view)
-        }
-
-        decorView.addView(fullscreenContainer)
-        fullscreen = true
-    }
-
-    private fun exitFullscreen(activity: Activity) {
-        val view = view ?: return
-        fullscreenContainer?.removeView(view)
-        fullscreenContainer?.parent?.let {
-            (it as ViewGroup).removeView(fullscreenContainer)
-        }
-
-        originalParent?.addView(view, originalLayoutParams)
-        activity.requestedOrientation = originalOrientation
-
-        fullscreen = false
-        fullscreenContainer = null
-    }
-
-    fun isFullscreen() = fullscreen
-
-    fun release() {
-        player?.release()
-        player = null
-        view = null
-    }
+  }
 }
+
+/**
+ * A custom VideoView that integrates with VlcPlayer.
+ */
+class VideoView constructor(private val reactContext: ReactContext) : FrameLayout(reactContext) {
+  private var player: VlcPlayer? = null
+  private var isFullscreen = false
+
+  init {
+    layoutParams = LayoutParams(
+      LayoutParams.MATCH_PARENT,
+      LayoutParams.MATCH_PARENT
+    )
+    player = VlcPlayer(context, VideoSource(uri = "")) // blank source initially
+    addView(player)
+  }
+
+  fun toggleFullscreen(fullscreen: Boolean) {
+    val activity = reactContext.currentActivity ?: return
+    if (fullscreen == isFullscreen) return
+
+    if (fullscreen) {
+      enterFullscreen(activity)
+    } else {
+      exitFullscreen(activity)
+    }
+  }
+
+  private fun enterFullscreen(activity: Activity) {
+    val originalOrientation = activity.requestedOrientation
+    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    (parent as? ViewGroup)?.removeView(this)
+
+    val decorView = activity.window.decorView as ViewGroup
+    val container = FrameLayout(context).apply {
+      layoutParams = LayoutParams(
+        LayoutParams.MATCH_PARENT,
+        LayoutParams.MATCH_PARENT
+      )
+      addView(this@VideoView)
+    }
+    decorView.addView(container)
+    isFullscreen = true
+  }
+
+  private fun exitFullscreen(activity: Activity) {
+    val decorView = activity.window.decorView as ViewGroup
+    val container = parent as? FrameLayout ?: return
+
+    container.removeView(this)
+    decorView.removeView(container)
+
+    // Switch orientation back if needed
+    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+    isFullscreen = false
+  }
+
+  fun release() {
+    player?.release()
+    player = null
+  }
+}
+
+/**
+ * Represents a VLC video source.
+ */
+data class VideoSource(
+  val uri: String,
+  val time: Long = 0
+)
